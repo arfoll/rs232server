fe6974f999467adfded8ecb20028e697734fc055
Brendan Le Foll <brendan@fridu.org>
2011-11-24 01:56:56 +0100

    split ampserver and ampserver dbus

--- ampserver.py
+++ ampserver.py
@@ -17,74 +17,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import gobject
-import dbus
-import dbus.service
 from dbus.mainloop.glib import DBusGMainLoop
-from ampserver_queue import AmpServerQueue
-
-AMPSERVER_BUS_NAME = 'uk.co.madeo.ampserver'
-AMPSERVER_BUS_PATH = '/uk/co/madeo/ampserver'
-
-class AmpService(dbus.service.Object):
-    def __init__(self):
-      bus_name = dbus.service.BusName(AMPSERVER_BUS_NAME, bus=dbus.SystemBus())
-      dbus.service.Object.__init__(self, bus_name, AMPSERVER_BUS_PATH)
-      self.queue = AmpServerQueue()
-
-    @dbus.service.method(AMPSERVER_BUS_NAME)
-    def mute(self):
-      self.queue.add('mute')
-
-    @dbus.service.method(AMPSERVER_BUS_NAME)
-    def unmute(self):
-      self.queue.add('unmute')
-
-    @dbus.service.method(AMPSERVER_BUS_NAME)
-    def poweron(self):
-      self.queue.add('poweron')
-
-    @dbus.service.method(AMPSERVER_BUS_NAME)
-    def poweroff(self):
-      self.queue.add('poweroff')
-
-    @dbus.service.method(AMPSERVER_BUS_NAME, in_signature='i')
-    def volumedown(self, db):
-      for i in range(0, db):
-        self.queue.add('voldown')
-
-    @dbus.service.method(AMPSERVER_BUS_NAME, in_signature='i')
-    def volumeup(self, db):
-      for i in range(0, db):
-        self.queue.add('volup')
-
-    @dbus.service.method(AMPSERVER_BUS_NAME, out_signature='i')
-    def getvolume(self):
-      self.queue.add('volup', True)
-      return int(self.queue.add('voldown', True))
-
-    @dbus.service.method(AMPSERVER_BUS_NAME)
-    def setinputvideo1(self):
-      self.queue.add('video1')
-
-    @dbus.service.method(AMPSERVER_BUS_NAME)
-    def setinputcdaux(self):
-      self.queue.add('cdaux')
-
-    @dbus.service.method(AMPSERVER_BUS_NAME, out_signature='s')
-    def getsversion(self):
-      return self.queue.add('sversion', True)
-
-    @dbus.service.method(AMPSERVER_BUS_NAME, out_signature='s')
-    def getpversion(self):
-      return self.queue.add('pversion', True)
-
-    @dbus.service.method(AMPSERVER_BUS_NAME)
-    def clear(self):
-      self.queue.add('clear', True)
-
-    @dbus.service.method(AMPSERVER_BUS_NAME, out_signature='b')
-    def check(self):
-      return True
+from ampserver_dbus import AmpService
 
 DBusGMainLoop(set_as_default=True)
 ampserv = AmpService()
