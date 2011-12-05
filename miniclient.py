78de48393e21d001ea75d7e358a2bbb2833965af
Brendan Le Foll <brendan@fridu.org>
2011-12-06 00:37:27 +0100

    add miniclient for xbmc scripting

---
+++ miniclient.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python2
+
+# Copyright (C) 2011 Brendan Le Foll <brendan@fridu.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import gobject
+gobject.threads_init()
+from dbus import glib
+glib.init_threads()
+import dbus
+import sys
+
+AMPSERVER_BUS_NAME = 'uk.co.madeo.ampserver'
+AMPSERVER_BUS_PATH = '/uk/co/madeo/ampserver'
+AMOUNT = 1
+
+bus = dbus.SystemBus()
+amp = bus.get_object(AMPSERVER_BUS_NAME, AMPSERVER_BUS_PATH)
+iface = dbus.Interface(amp, AMPSERVER_BUS_NAME)
+
+if (sys.argv[1] == "up"):
+  iface.volumeup(AMOUNT)
+else:
+  iface.volumedown(AMOUNT)
+
