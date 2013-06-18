c365508c48e95ab98c4d39012e930516c42b6d09
Brendan Le Foll <brendan@fridu.org>
2013-06-18 20:10:46 +0100

    add some more lgtv controls, basic inputs, energy control and backlight

--- rs232modules/lgtv_cmds.py
+++ rs232modules/lgtv_cmds.py
@@ -23,10 +23,23 @@ commands = {
 #power
   'poweroff'      : "ka 00 00",
   'poweron'       : "ka 00 01",
-  'powerstatus'   : "ka 00 ff"
+  'powerstatus'   : "ka 00 ff",
+# energy saving
+  'eneroff'       : "jq 00 00",
+  'enermin'       : "jq 00 01",
+  'enermed'       : "jq 00 02",
+  'enermax'       : "jq 00 03",
+  'enerscreenoff' : "jq 00 05",
+# backlight
+  'backlightoff'  : "mg 00 00",
+  'backlightfull' : "mg 00 64",
+# input
+  'hdmi'          : "xb 00 90",
+  'component'     : "xb 00 40"
 }
 
 responses = {
+  # power ACK
   "off"           : "a 00 OK00x",
   "on"            : "a 01 OK01x"
 }
