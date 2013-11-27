dfea8c9ee34620aaf64db6808a40370f5623a34e
Brendan Le Foll <brendan@fridu.org>
2013-11-27 20:33:00 +0000

    Initial guess of what an Arcam service would look like

---
+++ rs232modules/ArcamService.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python2
+
+# Copyright (C) 2011,2012,2013 Brendan Le Foll <brendan@fridu.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import dbus.service
+from dbus.mainloop.glib import DBusGMainLoop
+from SerialController import SerialController
+from BaseService import BaseService
+
+import arcam_cmds
+
+ARCAMSERVICE_IFACE = 'uk.co.madeo.rs232server.arcam'
+ARCAMSERVICE_OBJ_PATH = '/uk/co/madeo/rs232server/arcam'
+
+DELAY = 1
+BAUD_RATE = 38400
+BYTESIZE = 8
+READVAL = 10
+
+class ArcamService(BaseService):
+
+  def __init__(self, tty, bus_name):
+    BaseService.__init__(self, bus_name, ARCAMSERVICE_OBJ_PATH, tty, BAUD_RATE, READVAL, arcam_cmds)
+
+  @dbus.service.method(ARCAMSERVICE_IFACE, in_signature='sib', out_signature='s')
+  def send_cmd(self, cmd, repeat, check):
+    self.logger.debug("sent command : %s", cmd)
+    if cmd == "help":
+      self.logger.debug("Getting help!")
+      return self.help()
+    if (check):
+      return self.queue.add(arcam_cmds.commands[cmd], check)
+    for i in range(0, repeat):
+      self.queue.add(arcam_cmds.commands[cmd], check)
+    return ""
---
+++ rs232modules/arcam_cmds.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python2
+
+# Copyright (C) 2011,2012,2013 Brendan Le Foll <brendan@fridu.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+commands = {
+#volume
+  'voldown'      : "\x21\x01\x08\x02\x10\x11\x0D",
+  'volup'        : "\x21\x01\x08\x02\x10\x10\x0D"
+}
+
+responses = {
+}
