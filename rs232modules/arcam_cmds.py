ab60a4c40eaa96a96654c398f91777589b4ce857
Rob Sharp <qnm@fea.st>
2014-02-16 14:35:07 +1100

    Add extra debugging to the server, add main arcam commands

--- rs232modules/arcam_cmds.py
+++ rs232modules/arcam_cmds.py
@@ -15,10 +15,53 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+start = "\x50\x43\x5f" # PC_
+end = "\x0d" # <CR>
+
+zone_1 = "\x31"
+zone_2 = "\x32"
+
+option_0 = "\x30"
+option_1 = "\x31"
+option_2 = "\x32"
+option_3 = "\x33"
+option_4 = "\x34"
+option_5 = "\x35"
+option_6 = "\x36"
+option_7 = "\x37"
+option_8 = "\x38"
+option_9 = "\x39"
+
 commands = {
 #volume
-  'voldown'      : "\x21\x01\x08\x02\x10\x11\x0D",
-  'volup'        : "\x21\x01\x08\x02\x10\x10\x0D"
+  'voldown':  start + "\x2f" + zone_1 + option_0 + end,
+  'volup':    start + "\x2f" + zone_1 + option_1 + end,
+  'mute':     start + "\x2e" + zone_1 + option_0 + end,
+  'unmute':   start + "\x2e" + zone_1 + option_1 + end,
+#power
+  'poweron':  start + "\x2a" + zone_1 + option_1 + end,
+  'poweroff': start + "\x2a" + zone_1 + option_0 + end,
+#inputs
+  'dvd':      start + "\x31" + zone_1 + option_0 + end,
+  'sat':      start + "\x31" + zone_1 + option_1 + end,
+  'av':       start + "\x31" + zone_1 + option_2 + end,
+  'pvr':      start + "\x31" + zone_1 + option_3 + end,
+  'vcr':      start + "\x31" + zone_1 + option_4 + end,
+  'cd':       start + "\x31" + zone_1 + option_5 + end,
+  'fm':       start + "\x31" + zone_1 + option_6 + end,
+  'am':       start + "\x31" + zone_1 + option_7 + end,
+  'dvda':     start + "\x31" + zone_1 + option_8 + end,
+#two-channel modes
+  "pl2movie":  start + "\x34" + zone_1 + option_0 + end,
+  "pl2music":  start + "\x34" + zone_1 + option_1 + end,
+  "pl2xmovie": start + "\x34" + zone_1 + option_3 + end,
+  "pl2xmusic": start + "\x34" + zone_1 + option_4 + end,
+  "neo6cinema":start + "\x34" + zone_1 + option_7 + end,
+  "neo6music": start + "\x34" + zone_1 + option_8 + end,
+#surround modes
+  "stereo":    start + "\x35" + zone_1 + "\x2f" + end,
+  "dd/dtsmode":start + "\x35" + zone_1 + option_0 + end,
+  "pl2mode":   start + "\x35" + zone_1 + option_2 + end
 }
 
 responses = {
